d925369c7ddd850e908245011efb56e6b8df7164
Kewb1 <160376011+Kewb1@users.noreply.github.com>
2025-03-14 00:18:00 -0500

    Add files via upload

---
+++ Mtsu2025DBMS/backend/routes/recipe.route.js
@@ -0,0 +1,13 @@
+import express from "express";
+import mongoose, { get } from "mongoose";
+import Recipe from "../models/recipe.model.js";
+import { getRecipes, createRecipe, updateRecipe, deleteRecipe} from "../controllers/recipe.controller.js";
+
+const router = express.Router();
+
+router.get("/", getRecipes);
+router.post("/", createRecipe);
+router.put("/:id", updateRecipe);
+router.delete("/:id" , deleteRecipe);
+
+export default router;
